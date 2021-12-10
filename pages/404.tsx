d10809a00690476b695a76ffea61e859a9570cae
Matteu <matteugt@gmail.com>
2021-12-10 14:51:13 +0800

    Fix 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { NextPage } from "next";
 import BlankLayout from "./layouts/BlankLayout";
-import MSCCard from "../components/MSCCard";
-import MSCButton from "../components/MSCButton";
+import { HCButton, HCCard } from "../components";
 
 const NotFound: NextPage = () => {
     return (
         <BlankLayout title="404 Page Not Found" description="404 - Page Not Found">
             <section className="absolute-center">
                 <div className="page-error">
-                    <MSCCard title="404" description="Page not found...">
-                        <MSCButton link="/" type="filled">Return home</MSCButton>
-                    </MSCCard>
+                    <HCCard title="404" description="Page not found...">
+                        <HCButton link="/" type="filled">Return home</HCButton>
+                    </HCCard>
                 </div>
             </section>
         </BlankLayout>
