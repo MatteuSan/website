2893f2b7ef5db431c83d56bb92db8f71147cab64
Matteu <matteugt@gmail.com>
2024-02-23 03:16:55 +0800

    Config bruh moment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,9 +10,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <link
-          href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp&display=optional"
-          rel="stylesheet"/>
         <Head/>
         <body>
         <Main/>
--- pages/layouts/DefaultLayout.tsx
+++ pages/layouts/DefaultLayout.tsx
@@ -31,31 +31,35 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({ title, description, hasHe
     <>
       <Head>
         <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
 
         <title>{ title } - { site.name }</title>
         <meta property="description"
-              content={ description || `UX Engineer. Creating a meaningful bridge from software to user.` } />
+              content={ description || `UX Engineer. Creating a meaningful bridge from software to user.` }/>
 
-        <link rel="apple-touch-icon" sizes="180x180" href={ `${ site.url }/img/favicon.png` } />
-        <link rel="icon" type="img/png" sizes="32x32" href={ `${ site.url }/img/favicon.png` } />
-        <link rel="icon" type="img/png" sizes="16x16" href={ `${ site.url }/img/favicon.png` } />
+        <link rel="apple-touch-icon" sizes="180x180" href={ `${ site.url }/img/favicon.png` }/>
+        <link rel="icon" type="img/png" sizes="32x32" href={ `${ site.url }/img/favicon.png` }/>
+        <link rel="icon" type="img/png" sizes="16x16" href={ `${ site.url }/img/favicon.png` }/>
 
-        <meta property="og:title" content={ `${ title } - ${ site.name }` } />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp&display=optional"
+          rel="stylesheet"/>
+
+        <meta property="og:title" content={ `${ title } - ${ site.name }` }/>
         <meta property="og:description"
-              content={ description || `UX Engineer. Creating a meaningful bridge from software to user.` } />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content={ `${ site.url }/img/favicon.png` } />
-        <meta property="og:url" content={ site.url } />
+              content={ description || `UX Engineer. Creating a meaningful bridge from software to user.` }/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:image" content={ `${ site.url }/img/favicon.png` }/>
+        <meta property="og:url" content={ site.url }/>
 
-        <meta name="twitter:title" content={ `${ title } - ${ site.name }` } />
+        <meta name="twitter:title" content={ `${ title } - ${ site.name }` }/>
         <meta name="twitter:description"
-              content={ description || `UX Engineer. Creating a meaningful bridge from software to user.` } />
-        <meta name="twitter:image" content={ `${ site.url }/img/favicon.png` } />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content={ site.twitter } />
+              content={ description || `UX Engineer. Creating a meaningful bridge from software to user.` }/>
+        <meta name="twitter:image" content={ `${ site.url }/img/favicon.png` }/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:site" content={ site.twitter }/>
 
-        <meta name="theme-color" content={ site.themeColor } />
+        <meta name="theme-color" content={ site.themeColor }/>
 
       </Head>
       <HCHeader title={ site.name } actionSection={
