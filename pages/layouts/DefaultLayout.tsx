3da88f9856ad5f5c761ab555f6858cf63e185213
Matteu <matteugt@gmail.com>
2024-03-14 04:56:24 +0800

    Update og image

--- pages/layouts/DefaultLayout.tsx
+++ pages/layouts/DefaultLayout.tsx
@@ -49,7 +49,7 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({ title, description, hasHe
         <meta property="og:description"
               content={ description || `UX Engineer. Creating a meaningful bridge from software to user.` }/>
         <meta property="og:type" content="website"/>
-        <meta property="og:image" content={ `${ site.url }/img/favicon.png` }/>
+        <meta property="og:image" content={ `${ site.url }/img/og-image-home.png` }/>
         <meta property="og:url" content={ site.url }/>
 
         <meta name="twitter:title" content={ `${ title } - ${ site.name }` }/>
