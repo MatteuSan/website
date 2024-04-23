d4f9eacf54aac64e2add50a3a630ad97f8c84998
Matteu <matteugt@gmail.com>
2024-04-23 15:21:10 +0800

    Add item filtering from url params

--- pages/tools/index.tsx
+++ pages/tools/index.tsx
@@ -14,46 +14,17 @@ const cleanedItems = itemsWithSlug.concat(itemsWithoutSlug);
 const validToolTags = ['Design System', 'Library', 'Open Source'];
 
 const ToolsPage: NextPage = () => {
-  const [filter, setFilter] = useState('all')
+  const router = useRouter();
+  const [filter, setFilter] = useState<string>(router.query?.filter ? router.query?.filter.toString() : 'all');
   const filteredItems = filter !== 'all' ? cleanedItems.filter((item) => item?.tags.includes(filter ?? '')) : cleanedItems;
 
+  console.log(router.query.filter);
+
   const handleFilter = (event: any, targetFilter?: string) => {
-    // console.log(event.target.value ?? targetFilter);
     setFilter(event.target.value ?? targetFilter);
   }
 
   return (
-    <DefaultLayout title="TOOLS" heroTitle="Tools"
-                   heroSubtitle="Empowering teams (and you!) to build better user experiences." hasHero>
-      <section className="flex flow-row wrap-none jc-space-between ai-center gap-sm">
-        <h3 className="subtitle">
-          { filteredItems.length != 0 ? `Found ${ filteredItems.length } ${ filteredItems.length >= 2 ? 'items' : 'item' }.` : (
-            <span style={ { color: 'rgba(0,0,0,0)' } } aria-hidden="true">Hidden text</span>) }
-        </h3>
-        <label className="ms-select-field" style={ { maxWidth: 'max-content' } }>
-          <select className="ms-select-field__input" name="tags" onChange={ handleFilter }>
-            <option value="all">Filter</option>
-            { validToolTags.map((tag, key) => (
-              <option key={ key } value={ tag }>{ tag }</option>
-            )) }
-          </select>
-        </label>
-      </section>
-      <section className="content-section">
-        <div className="grid cols-1 @medium:cols-2 @large:cols-3 gap-lg" id="projects">
-          <>
-            { filteredItems.length != 0 ? filteredItems.map((item: any, key: any) => {
-              return (
-                <MSInfoCard key={ key } reference={ key } item={ item } linkBase="tools"/>
-              );
-            }) : (
-              <div className="grid pi-center p-2xl border-xs start-1 end-3">
-                <p className="subtitle">No items found.</p>
-              </div>
-            ) }
-          </>
-        </div>
-      </section>
     <DefaultLayout title="TOOLS" description="Empowering teams (and you!) to build better user experiences." hasHero>
       <MSHero title="Tools" subtitle="Empowering teams (and you!) to build better user experiences." />
       <MainContent>
@@ -63,6 +34,7 @@ const ToolsPage: NextPage = () => {
               <span style={ { color: 'rgba(0,0,0,0)' } } aria-hidden="true">Hidden text</span>) }
           </h3>
           <label className="ms-select-field" style={ { maxWidth: 'max-content' } }>
+            <select className="ms-select-field__input" name="tags" defaultValue={filter} onChange={ handleFilter }>
               <option value="all">Filter</option>
               { validToolTags.map((tag, key) => (
                 <option key={ key } value={ tag }>{ tag }</option>
--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -13,47 +13,18 @@ const itemsWithoutSlug = works.filter((item) => item?.slug == null);
 
 let cleanedItems = itemsWithSlug.concat(itemsWithoutSlug);
 
-const validWorkTags = ['Open Source', 'Website', 'Web App'];
+const validWorkTags = ['Design', 'Development', 'Open Source', 'Website', 'Web App'];
 
 const WorkPage: NextPage = () => {
-  const [filter, setFilter] = useState('all')
+  const router = useRouter();
+  const [filter, setFilter] = useState<string>(router.query?.filter ? router.query?.filter.toString() : 'all');
   const filteredItems = filter !== 'all' ? cleanedItems.filter((item) => item?.tags.includes(filter ?? '')) : cleanedItems;
 
   const handleFilter = (event: any, targetFilter?: string) => {
-    // console.log(event.target.value ?? targetFilter);
     setFilter(event.target.value ?? targetFilter);
   }
 
   return (
-    <DefaultLayout title="WORK" heroTitle="Works" heroSubtitle="Projects made for clients, and for myself :)" hasHero>
-      <section className="flex flow-row wrap-none jc-space-between ai-center gap-sm">
-        <h3 className="subtitle">
-          { filteredItems.length != 0 ? `Found ${ filteredItems.length } ${ filteredItems.length >= 2 ? 'items' : 'item' }.` : (<span style={{ color: 'rgba(0,0,0,0)' }} aria-hidden="true">Hidden text</span>) }
-        </h3>
-        <label className="ms-select-field" style={{ maxWidth: 'max-content' }}>
-          <select className="ms-select-field__input" name="tags" onChange={ handleFilter }>
-            <option value="all">Filter</option>
-            { validWorkTags.map((tag, key) => (
-              <option key={ key } value={ tag }>{ tag }</option>
-            )) }
-          </select>
-        </label>
-      </section>
-      <section className="content-section">
-        <MSAnimatedSection delay={ 0 } stagger={ 0.2 } className="grid cols-1 @medium:cols-2 @large:cols-3 gap-lg" id="projects">
-          { filteredItems.length != 0 ? filteredItems.map((item: any, key: number) => {
-            return (
-              <MSInfoCard key={ key } reference={ key } item={ item } linkBase="work"/>
-            );
-          }) :
-            (
-              <div className="grid pi-center p-2xl border-xs start-1 end-3">
-                <p className="subtitle">No items found.</p>
-              </div>
-            )
-          }
-        </MSAnimatedSection>
-      </section>
     <DefaultLayout title="WORK" description="Projects made for clients, and for myself :)" hasHero>
       <MSHero title="Works" subtitle="Projects made for clients, and for myself :)" />
       <MainContent>
@@ -63,6 +34,7 @@ const WorkPage: NextPage = () => {
               <span style={ { color: 'rgba(0,0,0,0)' } } aria-hidden="true">Hidden text</span>) }
           </h3>
           <label className="ms-select-field" style={ { maxWidth: 'max-content' } }>
+            <select className="ms-select-field__input" name="tags" defaultValue={filter} onChange={ handleFilter }>
               <option value="all">Filter</option>
               { validWorkTags.map((tag, key) => (
                 <option key={ key } value={ tag }>{ tag }</option>
