0240d2045d6e70cf5eedbb756b56cab6ab042e92
Matteu <matteugt@gmail.com>
2024-04-23 15:19:44 +0800

    Update layouts

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NextPage } from 'next';
-import DefaultLayout from "../../layouts/DefaultLayout";
+import { DefaultLayout, MainContent } from "../../layouts/DefaultLayout";
 
 const BlogMainPage: NextPage = () => {
   return (
@@ -11,6 +11,14 @@ const BlogMainPage: NextPage = () => {
           <h3 className="subtitle">Good things come to those who wait</h3>
         </div>
       </div>
+      <MainContent>
+        <div style={ { width: '100%', height: 'calc(100vh - (88.65px * 1.55))' } } className="grid pi-center">
+          <div className="flex flow-column jc-center ai-center">
+            <h1 className="supertitle">Coming soon.</h1>
+            <h3 className="subtitle">Good things come to those who wait</h3>
+          </div>
+        </div>
+      </MainContent>
     </DefaultLayout>
   );
 }
