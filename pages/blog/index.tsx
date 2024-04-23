07a959d3d2e4f95ca81117077816263df0cc83ae
Matteu <matteugt@gmail.com>
2024-04-23 15:19:58 +0800

    Update layouts

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -5,12 +5,6 @@ import { DefaultLayout, MainContent } from "../../layouts/DefaultLayout";
 const BlogMainPage: NextPage = () => {
   return (
     <DefaultLayout title="BLOG" description="Coming soon.">
-      <div style={{ width: '100%', height: 'calc(100vh - (88.65px * 1.55))' }} className="grid pi-center">
-        <div className="flex flow-column jc-center ai-center">
-          <h1 className="supertitle">Coming soon.</h1>
-          <h3 className="subtitle">Good things come to those who wait</h3>
-        </div>
-      </div>
       <MainContent>
         <div style={ { width: '100%', height: 'calc(100vh - (88.65px * 1.55))' } } className="grid pi-center">
           <div className="flex flow-column jc-center ai-center">
