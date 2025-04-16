c54dd4a0a4b7ad6375c560b2568862b82ccb0f90
Matteu <matteugt@gmail.com>
2025-04-17 02:40:55 +0800

    Dynamically enable lenis scroll on a per-page basis

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,13 @@ import { gsap } from 'gsap';
 
 import type { AppProps } from 'next/app';
 import '../scss/main.scss';
+import { useRouter } from "next/router";
 
 function App({ Component, pageProps }: AppProps) {
   const lenisRef = useRef<any>();
+  const currentRoute = `${useRouter().pathname}`;
+
+  const ALLOWED_ROUTES = ['/', '/about', '/work', '/tools'];
 
   useEffect(() => {
     function update(time: any) {
@@ -16,6 +20,12 @@ function App({ Component, pageProps }: AppProps) {
     gsap.ticker.add(update);
     return () => gsap.ticker.remove(update);
   }, []);
+
+  if (!ALLOWED_ROUTES.includes(currentRoute)) {
+    return (
+      <Component { ...pageProps } />
+    );
+  }
 
   return (
     <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
