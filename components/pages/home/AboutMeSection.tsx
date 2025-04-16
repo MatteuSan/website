d8cfe18e2967b121eb75db693f267919ebcc0bb4
Matteu <matteugt@gmail.com>
2025-04-16 14:11:33 +0800

    Remove dead code

--- components/pages/home/AboutMeSection.tsx
+++ components/pages/home/AboutMeSection.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { parallaxExit } from "@/lib/gsap";
 
 interface AboutMeSectionProps {}
 
