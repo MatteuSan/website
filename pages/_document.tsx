2ddab91ce800bd50a131c720fd1dd1d2533b7999
Matteu <matteugt@gmail.com>
2025-04-16 14:12:18 +0800

    Add hard coded imports for fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,10 +10,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head/>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          {/* @ts-expect-error */}
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Noto+Serif+Display:ital,wdth,wght@0,62.5..100,100..900;1,62.5..100,100..900&display=swap"
+            rel="stylesheet"/>
+        </Head>
         <body>
-          <Main/>
-          <NextScript/>
+        <Main/>
+        <NextScript/>
         </body>
       </Html>
     );
