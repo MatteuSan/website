a2012f2d3da5f6d09cd6994c6ca8ff33f5a96805
Matteu <matteugt@gmail.com>
2025-04-16 15:39:16 +0800

    Adjust lenis scroll

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { appWithTranslation } from 'next-i18next';
-import { ReactLenis, useLenis } from 'lenis/react';
+import { ReactLenis } from 'lenis/react';
+import { gsap } from 'gsap';
 
 import type { AppProps } from 'next/app';
 import '../scss/main.scss';
 
 function App({ Component, pageProps }: AppProps) {
+  const lenisRef = useRef<any>();
+
+  useEffect(() => {
+    function update(time: any) {
+      lenisRef.current?.lenis?.raf(time * 550)
+    }
+    gsap.ticker.add(update);
+    return () => gsap.ticker.remove(update);
+  }, []);
+
   return (
-    <ReactLenis root>
+    <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
       <Component { ...pageProps } />
     </ReactLenis>
   );
