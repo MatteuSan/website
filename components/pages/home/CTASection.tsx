e63775587e63a47e3f798fca601d86a617db4efe
Matteu <matteugt@gmail.com>
2025-04-16 15:39:24 +0800

    Add CTASection

---
+++ components/pages/home/CTASection.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { MSButton } from "@/components";
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { TextPlugin } from "gsap/dist/TextPlugin";
+
+import { SiGithub, SiLinkerd } from "@icons-pack/react-simple-icons";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, TextPlugin);
+
+interface CTASectionProps {}
+
+const CTASection: React.FC<CTASectionProps> = () => {
+  const ctaSectionRef = React.useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    const contentTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.lead-text',
+        start: 'top 90%',
+        end: 'bottom 60%',
+        scrub: true,
+        markers: true,
+        toggleActions: 'play pause resume reset',
+      }
+    });
+
+    const initialState = () => {
+      gsap.set(ctaSectionRef.current, {
+        opacity: 0,
+        y: 30
+      });
+
+      gsap.set('.content', {
+        opacity: 0,
+      });
+
+      gsap.set('.cta__actions .ms-button', {
+        opacity: 0,
+        y: 30,
+      });
+    }
+
+    const enterAnimation = () => {
+      contentTl.to(ctaSectionRef.current, {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+      });
+
+      contentTl.to('.content', {
+        opacity: 1,
+        duration: 3,
+      });
+
+      const CTA_ACTIONS = gsap.utils.toArray('.cta__actions .ms-button');
+
+      CTA_ACTIONS.forEach((button: any) => {
+        contentTl.to(button, {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          stagger: {
+            amount: 0.05
+          }
+        }, '-=0.5');
+      });
+    }
+
+    const exitAnimation = () => {
+      // Exit animations
+    }
+
+    initialState();
+    enterAnimation();
+    exitAnimation();
+  }, { scope: ctaSectionRef });
+
+  return (
+    <section className="constrained h-half-screen grid pi-center my-4xl @large:my-6xl" ref={ctaSectionRef}>
+      <section>
+        <div className="mb-lg">
+          <h2 className="lead-text family-supertitle size-2xl @medium:size-4xl @large:size-5xl line-height-condensed italic mb-sm">
+            Build the experience your users <span className="highlight">deserve</span>.
+          </h2>
+          <p className="content size-md @medium:size-lg weight-light">Have a project in mind? Let's bring it to life. From first sketch to final push—I'll help you ship something <span className="highlight">amazing</span>.</p>
+        </div>
+        <div className="cta__actions flex flow-row gap-md mt-lg">
+          <MSButton type="filled large" icon={ ['left', <EnvelopeIcon />] } link="mailto:matt@matteusan.com">Let's talk</MSButton>
+          <MSButton type="filled large" icon={ ['left', <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path></svg>] } link="https://linkedin/in/matteusan">Let's connect</MSButton>
+          <MSButton type="filled large" icon={ ['left', <SiGithub />] } link="https://github.com/MatteuSan">GitHub</MSButton>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default CTASection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,7 @@ import { DefaultLayout, MainContent } from '@/layouts/DefaultLayout';
 import {
   MSButton,
   MSHero,
-  MSTitleBar
 } from '@/components';
-
-import { EnvelopeIcon } from "@heroicons/react/24/outline";
-import { SiLinkerd } from "@icons-pack/react-simple-icons";
 
 import { works } from '@/constants/works';
 
@@ -17,6 +13,7 @@ import ServiceSection from "@/components/pages/home/ServiceSection";
 import TechnologySection from "@/components/pages/home/TechnologySection";
 import AboutMeSection from "@/components/pages/home/AboutMeSection";
 import WorksSection from "@/components/pages/home/WorksSection";
+import CTASection from "@/components/pages/home/CTASection";
 
 const workItemsWithSlug = works.filter((item) => item?.slug != null);
 const workItemsWithoutSlug = works.filter((item) => item?.slug == null);
@@ -28,22 +25,12 @@ const HomePage: NextPage = () => {
   return (
     <DefaultLayout title="HOME" hasHero>
       <MSHero title="MatteuSan" subtitle={<span>Creating bridges from <span className="italic family-supertitle">software</span> to <span className="italic family-supertitle">user</span>.</span>} action={ <MSButton type="filled large inverted" link="#about-me">Get to know me</MSButton> } />
-      <AboutMeSection />
-      <ServiceSection />
-      <TechnologySection />
-      <MainContent>
+      <MainContent constrained={false}>
+        <AboutMeSection />
+        <ServiceSection />
+        <TechnologySection />
         <WorksSection works={ new_works } />
-        <section className="grid pi-center my-4xl @large:my-6xl">
-          <section id="ready-to-collaborate">
-            <MSTitleBar typePreset="title">Ready to make the leap?</MSTitleBar>
-            <p style={ { margin: '1rem 0' } }>I'd be delighted to work on your project! Just click any of the contact buttons below and we'll keep in touch :)</p>
-            <div className="flex flow-row gap-md jc-center mt-lg">
-              <MSButton type="filled" icon={ ['left', <EnvelopeIcon />] } link="mailto:matt@matteusan.com"
-                        isDisabled>Email</MSButton>
-              <MSButton type="filled" icon={ ['left', <SiLinkerd />] } link="/linkedin" isDisabled>LinkedIn</MSButton>
-            </div>
-          </section>
-        </section>
+        <CTASection />
       </MainContent>
     </DefaultLayout>
   );
