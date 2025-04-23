31dbda614758ceb66efb9da42053b7b7ff013145
Matteu <matteugt@gmail.com>
2025-04-23 21:08:12 +0800

    Reconfigure lenis

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import type { AppProps } from 'next/app';
 import { Analytics } from "@vercel/analytics/react"
 import { appWithTranslation } from 'next-i18next';
-import { ReactLenis } from 'lenis/react';
-import { gsap } from 'gsap';
-import { AnimatePresence, MotionConfig } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
+import ReactLenis from 'lenis/react';
 
 import { MotionProvider } from '@/stores/accessibility';
 import '../scss/main.scss';
@@ -12,35 +11,15 @@ import '../scss/main.scss';
 
 function App({ Component, pageProps, router }: AppProps) {
   const [prefersReducedMotion, setPrefersReducedMotion] = useState(false);
-  const lenisRef = useRef<any>();
-  const currentRoute = `${router.pathname}`;
-
-  const ALLOWED_ROUTES = ['/', '/about', '/work', '/tools'];
-
-  useEffect(() => {
-    function update(time: any) {
-      lenisRef.current?.lenis?.raf(time * 550)
-    }
-    gsap.ticker.add(update);
-    return () => gsap.ticker.remove(update);
-  }, []);
-
-  if (!ALLOWED_ROUTES.includes(currentRoute)) {
-    return (
-      <Component { ...pageProps } />
-    );
-  }
 
   return (
     <MotionProvider value={{ prefersReducedMotion, setPrefersReducedMotion }}>
-      <MotionConfig>
-        <AnimatePresence mode="wait">
-          <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
-            <Component key={router.route} { ...pageProps } />
-            <Analytics />
-          </ReactLenis>
-        </AnimatePresence>
-      </MotionConfig>
+      <AnimatePresence mode="wait">
+        <ReactLenis root options={{ autoResize: true }}>
+          <Component key={router.route} { ...pageProps } />
+          <Analytics />
+        </ReactLenis>
+      </AnimatePresence>
     </MotionProvider>
   );
 }
