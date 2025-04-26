594f1fcc3bd3968643476054eabc3fd24d8d8e95
Matteu <matteugt@gmail.com>
2025-04-27 01:44:47 +0800

    Add stuff for the blog

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { DefaultLayout, MainContent } from "@/layouts/DefaultLayout";
+import { MSHero } from '@/components';
 
 const BlogMainPage: NextPage = () => {
   return (
-    <DefaultLayout title="BLOG" description="Coming soon.">
+    <DefaultLayout title="BLOG" description="Coming soon." hasHero>
+      <MSHero title="Blog" subtitle="All the things I'm learning, and software I'm building." />
       <MainContent>
         <div style={ { width: '100%', height: 'calc(100vh - (88.65px * 1.55))' } } className="grid pi-center">
           <div className="flex flow-column jc-center ai-center">
