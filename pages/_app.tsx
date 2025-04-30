6f9962a12b7589b0c06b31bf049c547af63a613f
Matteu <matteugt@gmail.com>
2025-04-30 15:09:26 +0800

    Remove scroll-jacking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,21 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import { AnimatePresence, domAnimation, LazyMotion, MotionConfig } from 'framer-motion';
-import ReactLenis from 'lenis/react';
 
 import '../scss/main.scss';
 
 function App({ Component, pageProps, router }: AppProps) {
   return (
-    <ReactLenis root options={{ autoResize: true }}>
+    <>
       <MotionConfig reducedMotion="user">
         <LazyMotion features={domAnimation}>
           <AnimatePresence mode="wait">
             <Component key={router.route} {...pageProps} />
-            <Analytics key="analytics" mode="production" />
           </AnimatePresence>
         </LazyMotion>
       </MotionConfig>
-    </ReactLenis>
+      <Analytics key="analytics" mode="auto" />
+    </>
   );
 }
 
