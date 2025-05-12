2a31add47b2d2b09d8875fa8058f5604a8bc45d0
Matteu <matteugt@gmail.com>
2025-05-12 19:39:10 +0800

    Remove Lenis

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { Analytics } from '@vercel/analytics/react';
 import { AnimatePresence, domAnimation, LazyMotion, MotionConfig } from 'framer-motion';
 
 import '../scss/main.scss';
-import ReactLenis from 'lenis/react';
 
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
@@ -17,15 +16,13 @@ gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 function App({ Component, pageProps, router }: AppProps) {
   return (
     <>
-      <ReactLenis root options={{ autoRaf: true }}>
-        <MotionConfig reducedMotion="user">
-          <LazyMotion features={domAnimation}>
-            <AnimatePresence mode="wait">
-              <Component key={router.route} {...pageProps} />
-            </AnimatePresence>
-          </LazyMotion>
-        </MotionConfig>
-      </ReactLenis>
+      <MotionConfig reducedMotion="user">
+        <LazyMotion features={domAnimation}>
+          <AnimatePresence mode="wait">
+            <Component key={router.route} {...pageProps} />
+          </AnimatePresence>
+        </LazyMotion>
+      </MotionConfig>
       <Analytics key="analytics" mode="auto" />
       <SpeedInsights />
     </>
