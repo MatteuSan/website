b59ebadb001dd4f2f3f0c1a1d496898ac909ab58
Matteu <matteugt@gmail.com>
2025-07-18 04:15:34 +0800

    Add new blog

--- constants/blogs.ts
+++ constants/blogs.ts
@@ -10,5 +10,11 @@ export const blogs = [
     description: 'Creating a low-level tool to build and manage design systems at scale.',
     publishedAt: '2025-05-31',
     slug: 'creating-sentro',
+  },
+  {
+    title: 'The Goldilocks Zone',
+    description: 'My thoughts on building imperfectly usable software.',
+    publishedAt: '2025-07-18',
+    slug: 'the-goldilocks-zone',
   }
 ];
